32c55c4333e3d06007ca242d0434e0c2458dfa12
thongtq <thongtqse173366@fpt.edu.vn>
2024-10-08 16:25:19 +0700

    feature: add header and footer

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,30 +1,48 @@
 <template>
-    <div>
-      <HeaderBarHomePage />
-      <div class="content">
-        <p>Home Page</p>
-      </div>
-      <FooterHomePage />
+  <div>
+    <AppHeader />
+    <div class="content">
+      <section class="champion">
+        <div v-for="champion in KOI_CHAMPION">
+          <div class="champion-card">
+            <img src="@/assets/images/grand-champ.png" alt="champion" class="champion-image" />
+            <div class="champion-details">
+              <h2 class="champion-title">{{ champion.prize }}</h2>
+              <p class="champion-info">{{ champion.cateogory }}</p>
+              <p class="champion-info">{{ champion.owner }}</p>
+              <p class="champion-info">{{ champion.dealer }}</p>
+              <p class="champion-info">{{ champion.breeder }}</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  </template>
-  
-  <script>
-  import HeaderBarHomePage from '@/components/HeaderBarHomePage.vue'
-  import FooterHomePage from '@/components/FooterHomePage.vue'
-  
-  export default {
-    name: 'HomePage',
-    components: {
-      HeaderBarHomePage,
-      FooterHomePage
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
+import { KOI_CHAMPION } from '@/constants/index.ts'
+
+export default {
+  name: 'HomePage',
+  components: {
+    AppHeader,
+    AppFooter
+  },
+  data() {
+    return {
+      KOI_CHAMPION
     }
   }
-  </script>
-  
-  <style scoped>
-  .content {
-    min-height: 60vh; 
-    padding: 20px;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.content {
+  min-height: 60vh;
+  padding: 20px;
+}
+</style>
