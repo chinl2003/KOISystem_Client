68fc0a9068dec8c0d28b43348a48dd38518f8b4b
chinl2003 <nguyenlchi220602@gmail.com>
2024-10-15 10:39:55 +0700

    Fix bug

--- vite.config.js
+++ vite.config.js
@@ -16,13 +16,6 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5162', 
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+    port: 3000
   }
 })
